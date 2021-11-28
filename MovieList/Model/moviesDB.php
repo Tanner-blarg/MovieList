80f72aa499befb0fd947e95487641e5ded8ff8cb
tanne <tanne@DESKTOP-9L43BLP.home>
2021-11-28 10:31:23 -0600

    added most of basic database stuff for login

--- MovieList/Model/moviesDB.php
+++ MovieList/Model/moviesDB.php
@@ -29,12 +29,7 @@ class moviesDB{
               VALUES (:first_name, :last_name, :email, :passwords_hashed)';
         $statement = $db->prepare($query);        
         $statement->bindValue(':first_name', $first_name);
-        $statement->bindValue(':middle_name', $middle_name);
         $statement->bindValue(':last_name', $last_name);
-        $statement->bindValue(':suffix', $suffix);
-        $statement->bindValue(':phone', $phone);
-        $statement->bindValue(':dob', $dob);
-        $statement->bindValue(':gender', $gender);
         $statement->bindValue(':email', $email);
         $statement->bindValue(':passwords_hashed', $password);
         $statement->execute();
